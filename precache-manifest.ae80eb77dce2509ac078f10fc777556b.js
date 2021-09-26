87516ea7c1c4491e05f66eaefbd1e9863ee8090f
Mayur Sojitra <mrkumar198021@gmail.com>
2021-09-26 14:08:08 +0530

    Updates

---
+++ precache-manifest.ae80eb77dce2509ac078f10fc777556b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6618388d468dec60f0c65f58fc12c24",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "978c0b08752ff87fb447",
+    "url": "/ToDoApp/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "373ceba909ac302e5c45",
+    "url": "/ToDoApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "978c0b08752ff87fb447",
+    "url": "/ToDoApp/static/js/2.e11599a2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ToDoApp/static/js/2.e11599a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "373ceba909ac302e5c45",
+    "url": "/ToDoApp/static/js/main.27a65222.chunk.js"
+  },
+  {
+    "revision": "9fc0b1595a2456769560",
+    "url": "/ToDoApp/static/js/runtime-main.a6d8360d.js"
+  }
+]);
