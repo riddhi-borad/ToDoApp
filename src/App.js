8bafdee7605da92e7b424682af8157407f99c347
Mayur Sojitra <mrkumar198021@gmail.com>
2021-09-26 18:52:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './Component/Header';
 import Add from './Component/Add';
-import View from './Component/View';
+import View from './Component/ViewList';
 import Edit from './Component/Edit';
 import {Provider} from './Component/Context';
 import {BrowserRouter as Router,Route,HashRouter} from 'react-router-dom';
@@ -11,12 +11,13 @@ function App() {
     {/* <Router> */}
       <Provider>
       
-        <Route exact  path='/' component={Header}></Route>
+        <Route  path='/' component={Header}></Route>
         <Route path='/add-list' component={Add}></Route>
+        </Provider>
         {/* <Route path='/edit-list/:id' component={Edit}></Route> */}
         <Route path='/view-list' component={View}></Route>
       
-      </Provider>
+      
       {/* </Router> */}
       </HashRouter>
   );
